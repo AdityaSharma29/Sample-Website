ad6f9733a20296c084ed4f24c3d918c94c1a486a
AdityaSharma29 <61984836+AdityaSharma29@users.noreply.github.com>
2020-06-26 21:53:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-
 window.onload = function () {
-
     function close() {
         document.getElementById("temp").style.display = "none";
     }
-
-    console.log("outside");
-
-    
     document.getElementById("close-btn").addEventListener("click", close);
 }
-
-
-
